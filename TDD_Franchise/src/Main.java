53de1e5ff4b632cc7b503643765c5a436a30b280
Aronson1 <LSA9@pitt.edu>
2015-12-04 18:05:54 -0500

    Adding User Input

--- TDD_Franchise/src/Main.java
+++ TDD_Franchise/src/Main.java
@@ -3,8 +3,16 @@
  */
 public class Main {
     public static void main(String [] args){
-        //hello
-        int x = 10;
+
+        System.out.println("Hello welcom to the coffee card tracker home!");
+        System.out.println("Please select from the following options \n " +
+                "1) Create an account\n " +
+                "2) View your card balance\n" +
+                "3) Buy a pastry for $2.00\n" +
+                "4) Buy a coffee for $1.00\n" +
+                "5) Quit");
+
+
 
     }
 }
